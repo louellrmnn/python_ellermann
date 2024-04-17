ee8a3d88c7794269e2bd759da8cdab22846e4c17
louellrmnn <ellermannlouise@gmail.com>
2024-04-17 16:40:56 +0200

    in progress

--- scrapper3.py
+++ scrapper3.py
@@ -1,7 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
 
-URL = 'https://www.scrapethissite.com/pages/simple/'
+URL = 'https://www.scrapethissite.com/pages/forms/?page_num=1'
 response = requests.get(URL)
 
 soup = BeautifulSoup(response.text, 'html.parser')
+
+   
+team_name = soup.find_all('td', class_='name')
+team_win = soup.find_all('td', class_='pct text-success')
+team_goal = soup.find_all('td', class_='ga') 
+
+
+for name, win, goal in zip(team_name, team_win, team_goal):
+    print(name.text, '-', win.text, '-', goal.text)
